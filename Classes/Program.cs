6d12b232de5646224cd2553e02a7bfc3f0271798
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-16 18:26:02 -0600

    completed exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myTruck = new Car();
+            myTruck.Make = "Toyota";
+            myTruck.Model = "Tacoma";
+            myTruck.Year = 2022;
+            Console.WriteLine($"Make: {myTruck.Make} \nModel: {myTruck.Model} \nYear: {myTruck.Year}");
         }
     }
 }
